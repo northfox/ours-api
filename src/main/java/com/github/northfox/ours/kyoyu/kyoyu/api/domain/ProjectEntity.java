3962571b1cbbc121c2fb8a48abcc573054c20fde
northfox <vulpes.engineer@gmail.com>
2019-08-25 18:46:42 +0900

    Add entities

---
+++ src/main/java/com/github/northfox/ours/kyoyu/kyoyu/api/domain/ProjectEntity.java
@@ -0,0 +1,41 @@
+package com.github.northfox.ours.kyoyu.kyoyu.api.domain;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.joda.time.DateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "trx_project")
+public class ProjectEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt = DateTime.now().toDate();
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt = DateTime.now().toDate();
+    @Column(name = "deleted_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date deletedAt;
+}
---
+++ src/test/java/com/github/northfox/ours/kyoyu/kyoyu/api/domain/TodoEntityTest.java
@@ -0,0 +1,19 @@
+package com.github.northfox.ours.kyoyu.kyoyu.api.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TodoEntityTest {
+
+    private TodoEntity sut;
+
+    @BeforeEach
+    void setup() {
+        sut = new TodoEntity();
+    }
+
+    @Test
+    void exists() {
+
+    }
+}
