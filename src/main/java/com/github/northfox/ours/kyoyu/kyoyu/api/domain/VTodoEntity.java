7509ca13345d6ac6819278382f6f8724fbc8f1b1
northfox <vulpes.engineer@gmail.com>
2019-08-25 19:26:14 +0900

    Add entity for view

---
+++ src/main/java/com/github/northfox/ours/kyoyu/kyoyu/api/domain/VTodoEntity.java
@@ -0,0 +1,55 @@
+package com.github.northfox.ours.kyoyu.kyoyu.api.domain;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.joda.time.DateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "view_todo")
+public class VTodoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_id")
+    private Integer projectId;
+    @Column(name = "project_title")
+    private String projectTitle;
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "status_id")
+    private Integer statusId = 0;
+    @Column(name = "status")
+    private String status;
+    @Column(name = "deadline")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date deadline;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt = DateTime.now().toDate();
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt = DateTime.now().toDate();
+    @Column(name = "done_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date doneAt;
+    @Column(name = "deleted_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date deletedAt;
+}
---
+++ src/test/java/com/github/northfox/ours/kyoyu/kyoyu/api/domain/VTodoEntityTest.java
@@ -0,0 +1,20 @@
+package com.github.northfox.ours.kyoyu.kyoyu.api.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class VTodoEntityTest {
+
+    private VTodoEntity sut;
+
+    @BeforeEach
+    void setup() {
+        sut = new VTodoEntity();
+    }
+
+    @Test
+    void exists() {
+
+    }
+
+}
