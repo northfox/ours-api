094425358091456207abf4fbc4e6697eb0e51a0e
northfox <vulpes.engineer@gmail.com>
2019-08-28 22:15:32 +0900

    Add finding project api

---
+++ src/main/java/com/github/northfox/ours/kyoyu/kyoyu/api/exception/ApplicationException.java
@@ -0,0 +1,9 @@
+package com.github.northfox.ours.kyoyu.kyoyu.api.exception;
+
+public class ApplicationException extends Exception {
+    public ApplicationException() {
+    }
+    public ApplicationException(String message) {
+        super(message);
+    }
+}
