bdc58b3e419d9dd57c0a9acb0c2d153acb171dd9
northfox <vulpes.engineer@gmail.com>
2019-11-28 07:31:37 +0900

    Add dummy API

--- src/main/java/com/github/northfox/ours/preventloss/api/controller/PreventLossRestApiController.java
+++ src/main/java/com/github/northfox/ours/preventloss/api/controller/PreventLossRestApiController.java
@@ -3,7 +3,9 @@ package com.github.northfox.ours.preventloss.api.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.annotation.RequestScope;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -16,6 +18,14 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @RequestScope
 @RequestMapping(value = "/preventloss/api/v1", produces = MediaType.APPLICATION_JSON_VALUE)
 public class PreventLossRestApiController {
+
+    // hello
+    @RequestMapping(value = "/hello", method = {RequestMethod.GET})
+    public ResponseEntity<String> apiV1Statuses() {
+        preCall();
+        return ResponseEntity.ok("hello, world");
+    }
+
     private void preCall() {
         String requestUri = ServletUriComponentsBuilder.fromCurrentRequestUri().toUriString();
         log.info(String.format("call api: [%s]", requestUri));
